18296c44bb223e8759af0a665a06099a64739709
Atta-E-Mustafa <attaemustafa442@gmail.com>
2024-10-13 22:10:17 +0500

    'Pushed'

---
+++ GoatSneakersApparels/app/src/main/java/com/guneet/goat_sneakersapparels/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.guneet.goat_sneakersapparels
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_user)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvLogin)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
